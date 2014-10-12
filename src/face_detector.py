bd4b1f341310076624824afa643867a59e12dd40
Henry Garnelo Prediger <enriquegarneloprediger@gmail.com>
2014-10-12 21:31:44 +0200

    Enhancements to detection functionality
    Adding a fixed position rectangle that changes color from red to green to notify when it is suitable to take a picture.
    Press space bar to take picture of detected face, saves the file in "data" directory under a dummy name.

--- src/face_detector.py
+++ src/face_detector.py
@@ -9,19 +9,24 @@ import freenect
 import cv2 as cv
 import frame_convert
 import os.path
+import numpy as np
 
 #Global variables
 keep_running = True
 tilt_angle=None
+r_color = (0,0,255)
+picture_flag=False
+
 #Window creation
 cv.namedWindow('RGB')
 
 #Path to .xml file
-cascPath = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +"/data/" + "haarcascade_frontalface.xml"
+data_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +"/data/"
+cascPath = data_path + "haarcascade_frontalface.xml"
 faceCascade = cv.CascadeClassifier(cascPath)
 
 def display_rgb(dev, data, timestamp):
-    global keep_running,tilt_angle
+    global keep_running,tilt_angle,r_color,picture_flag, data_path
     #Capturing frame by frame
     frame=frame_convert.video_cv(data)
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -32,13 +37,30 @@ def display_rgb(dev, data, timestamp):
         minNeighbors=5,
         minSize=(30, 30),
     )
+    height, width, depth = frame.shape
+    r_area = (180,180)
+    rx = (width-r_area[0])/2
+    ry = (height-r_area[1])/2
     #Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0),2)
+        #Uncomment to watch detection rectangle
+        #cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0),2)
+        if x>=rx and x+w<=rx+r_area[0] and y>=ry and y+h<=ry+r_area[1]:
+            r_color=(0,255,0)
+            picture_flag=True
+        else:
+            r_color=(0,0,255)
+            picture_flag=False
+    #Draw fixed rectangle in image
+    cv.rectangle(frame,(rx,ry),(rx+r_area[0],ry+r_area[1]),r_color,2)
     #Displaying the resulting frame
     cv.imshow('RGB', frame)
     #Key event handling + manual motor movement
     key = cv.waitKey(1)
+    if key== 32 and picture_flag:
+        print "WHISKY!"
+        picture = frame[y:y+h , x:x+w]
+        cv.imwrite(data_path+"test.png",picture)
     if key == 65362: #Up key
         if not tilt_angle+3>27:
             tilt_angle+=3
@@ -49,7 +71,7 @@ def display_rgb(dev, data, timestamp):
         keep_running = False
 
 def body(dev,ctx):
-    global last_time,tilt_angle
+    global tilt_angle
     #Initializes tilt_angle variable for motor control
     if tilt_angle==None:
         freenect.update_tilt_state(dev)
