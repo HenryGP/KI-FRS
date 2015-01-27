58a279d7bf6d36e86d84e3ce0520be4057b51bb5
henrygp <henrygp@henrygp>
2015-01-27 14:25:15 +0100

    -File creation -High-level methods header definition

---
+++ src/preprocessor.py
@@ -0,0 +1,26 @@
+"""
+    Pre-processor for images
+    Includes:
+    - Light-related algorithms
+    - Depth normalization algorithms
+    - Auxiliaries
+"""
+
+"""
+    Estimates light intensity in given RGB image
+    Based on the estimated value, processes the BW image
+"""
+def light_estimation(rgb_img):
+    pass
+
+"""
+    Applies retina modeling algorithm to BW image
+"""
+def retina_processing(bw_img):
+    pass
+
+"""
+    Normalizes depth image to avoid 'blank spots'
+"""
+def normalize_depth(depth_img):
+    pass
