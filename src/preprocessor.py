172d942bf67e7f6a894971aa4c59718563d97bdd
henrygp <henrygp@henrygp>
2015-02-18 10:32:16 +0100

    Depth distance estimation by using depth matrix raw values.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -6,11 +6,19 @@
     - Auxiliaries
 """
 
+import math
+import numpy as np
+from file_manager import Picture_Manager
+import cv2 as cv
+
+manager = Picture_Manager()
+
 """
     Estimates light intensity in given RGB image
     Based on the estimated value, processes the BW image
 """
 def light_estimation(rgb_img):
+    samples, labels = manager.get_samples("tr","mtx")
     pass
 
 """
@@ -20,7 +28,18 @@ def retina_processing(bw_img):
     pass
 
 """
-    Normalizes depth image to avoid 'blank spots'
+   Gets all depth matrixes taken from the samples directory and normalizes it
 """
-def normalize_depth(depth_img):
-    pass
+def normalize_depth(mode="tr"):
+    samples,labels=manager.get_samples(mode,"mtx")
+    samples = samples.astype(np.float32)
+    for i in xrange(samples.shape[0]):
+        matrix = samples[i]
+        for p in xrange(matrix.shape[0]):
+            matrix[p]=0.1236 * math.tan(float(matrix[p]) / 2842.5 + 1.1863) * 100 #in centimeters
+        samples[i]=matrix
+        print matrix.max(),matrix.min()
+    manager.save_samples(mode,"mtx",samples,labels)        
+        
+
+normalize_depth("tr")
