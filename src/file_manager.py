b71f727848284e179eeed691912269f32a97a80d
henrygp <henrygp@henrygp>
2015-03-18 18:11:07 +0100

    Sample Manager class modifications

--- src/file_manager.py
+++ src/file_manager.py
@@ -14,67 +14,57 @@ class Sample_Manager():
     """
     """Path to the multiple data being managed"""
     #Data path
-    data_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +"/data/"
+    data_path = "%s/data" % (os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
     #Detector path
-    detect_path = data_path + "detector/"
+    detect_path = "%s/detector/" % (data_path)
     #Images path
-    img_path = data_path + "img/"
+    img_path = "%s/img" % (data_path)
     #Training path
-    tr_path = img_path + "tr/"
+    tr_path = "%s/tr/" % (img_path)
     #Test path
-    ts_path = img_path + "ts/"
+    ts_path = "%s/ts/" %(img_path)
+    
     #.xml file with cascade detector for frontal faces
     faceCascade = cv.CascadeClassifier(detect_path + "haarcascade_frontalface.xml")
     
-    """Internal dir counters"""
-    img_ptr = 0; tr_ptr = 0; ts_ptr = 0 
-    
-    def __init__(self,mode="tr"):
-        """
-            Initializes counter for training and test files by reading
-            the data through the state file.
-        """
-        if mode=="tr": #Training mode
-            self.tr_counter = [x[0].split("/")[-1] for x in os.walk(self.tr_path)]
-            max = 0
-            for i in list(self.tr_counter):
-                try: 
-                    if int(i)>max: 
-                        max = int(i)
-                except:
-                    continue
-            self.tr_counter=max
-            self.new_sampling("tr")
-            self.tr_ptr=self.tr_counter
-            self.to_dir(self.tr_ptr, "tr")
+    def __init__(self):
+        """Training set"""
+        self.tr_counter = sorted([x[0].split("/")[-1] for x in os.walk(self.tr_path)])
+        if not self.tr_counter[-1]=="":
+            self.tr_counter = int(self.tr_counter[-1])
         else:
-            self.ts_counter = [x[0].split("/")[-1] for x in os.walk(self.ts_path)]
-            max = 0
-            for i in list(self.ts_counter):
-                try: 
-                    if int(i)>max: 
-                        max = int(i)
-                except:
-                    continue
-            self.ts_counter=max
-            self.new_sampling("ts")
-            self.ts_ptr=self.ts_counter
-            self.to_dir(self.ts_ptr, "ts")          
-            
-    """def __del__(self):"""
-    
+            self.tr_counter = 0
+        """Testing set"""
+        self.ts_counter = sorted([x[0].split("/")[-1] for x in os.walk(self.ts_path)])
+        if not self.ts_counter[-1]=="":
+            self.ts_counter = int(self.ts_counter[-1])
+        else:
+            self.ts_counter = 0
+         
+    def __del__(self):
+        """Deletes directories if empty"""
+        try:
+            files = sorted([x[0].split("/")[-1] for x in os.walk(self.tr_path+str(self.tr_counter)+"/")])
+            if len(files)==1:
+                os.removedirs(self.tr_path+str(self.tr_counter))
+        except:pass
+        ###
+        try:
+            files = sorted([x[0].split("/")[-1] for x in os.walk(self.ts_path+str(self.ts_counter)+"/")])
+            if len(files)==1:
+                os.removedirs(self.ts_path+str(self.ts_counter))
+        except: pass
+        
     def new_sampling(self,mode="tr"):
         """Creates new directory under the dir specified by 'mode'"""
         if mode == "tr": #Creates new folder for training
-            self.tr_counter += 1
             print "Path: ",self.tr_path+str(self.tr_counter)
+            self.tr_counter+=1
             os.makedirs(self.tr_path+str(self.tr_counter))
-            self.tr_ptr=self.tr_counter
         else: #Creates new folder for test
-            self.ts_counter += 1
             print "Path: ",self.ts_path+str(self.ts_counter)
+            self.ts_counter += 1
             os.makedirs(self.ts_path+str(self.ts_counter))
-            self.ts_ptr=self.ts_counter
         self.img_ptr = 0
         
     def store_samples(self,samples,mode="tr"):
@@ -82,10 +72,8 @@ class Sample_Manager():
             Gets the image samples (BW,RGB and Depth) and stores them in corresponding
             dir indicated by ptr. Images are named according to img_ptr value 
         """
-        print "tr pointer: ",self.tr_ptr
-        print "ts pointer: ",self.ts_ptr
-        if mode=="tr": path=self.tr_path+str(self.tr_ptr)+"/"
-        else: path=self.ts_path+str(self.ts_ptr)+"/"
+        if mode=="tr": path=self.tr_path+str(self.tr_counter)+"/"
+        else: path=self.ts_path+str(self.ts_counter)+"/"
         try:
             self.img_ptr += 1
             print "Path: ",path
@@ -97,46 +85,20 @@ class Sample_Manager():
         except:
             print "Images couldn't be saved"
     
-    def to_dir(self,dir_ptr,mode="tr"):
-        if mode == "tr":
-            if dir_ptr>self.tr_counter:
-                raise
-            else:
-                self.tr_ptr=dir_ptr
-                self.img_ptr =  [ int(f.split("_")[0]) for f in listdir(self.tr_path+str(dir_ptr)+"/") if isfile(join(self.tr_path+str(dir_ptr)+"/",f)) ]
-                if len(self.img_ptr)==0:
-                    self.img_ptr=0
-                else:
-                    self.img_ptr =  max(self.img_ptr)
-                print "Tr pointer: ",self.tr_ptr
-                print "Img pointer: ",self.img_ptr
-        else:
-            if dir_ptr>self.ts_counter:
-                raise
-            else:
-                self.ts_ptr=dir_ptr
-                self.img_ptr =  [ int(f.split("_")[0]) for f in listdir(self.ts_path+str(dir_ptr)+"/") if isfile(join(self.ts_path+str(dir_ptr)+"/",f)) ]
-                if len(self.img_ptr)==0:
-                    self.img_ptr=0
-                else:
-                    self.img_ptr =  max(self.img_ptr)
-                print "Ts pointer: ",self.ts_ptr
-                print "Img pointer: ",self.img_ptr
-
 class Picture_Manager():
     """
-        Class for images management for preprocessing and recognition
+        Class for image management for preprocessing and recognition
     """
     #Data path
-    data_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +"/data/"
+    data_path =  "%s/data/"%(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
     #Images path
-    img_path = data_path + "img/"
+    img_path = "%simg/" % (data_path)
     #Training path
-    tr_path = img_path + "tr/"
+    tr_path = "%str/" % (img_path)
     #Test path
-    ts_path = img_path + "ts/"
+    ts_path = "%sts/" % (img_path)
     #Recognition path
-    rec_path = data_path + "recognition/"
+    rec_path = "%srecognition/"%(data_path)
     
     def get_samples(self,mode="tr",type="bw"):
         if mode=="tr":
@@ -184,27 +146,24 @@ class Picture_Manager():
 
     def load_model(self,mode,num_components,threshold):
         if mode==1:
-            name = "eigenfaces.yml"
+            name = "eigenfaces.yaml"
             model = cv.createEigenFaceRecognizer(num_components,threshold)
-            try:
-                model.load(self.rec_path+name)
-            except:
-                print "There was no model"
         else:
-            name = "fisherfaces.yml"
+            name = "fisherfaces.yaml"
             model = cv.createFisherFaceRecognizer()
-            try:
-                model.load(self.rec_path+name)
-            except:
-                print "There was no model"       
+        try:
+            model.load(self.rec_path+name)
+        except:
+            print "There was no model"
         return model
         
     def save_model(self,mode,model):
         if mode==1:
-            name = "eigenfaces.yml"
+            name = "eigenfaces.yaml"
         else:
-            name = "fisherfaces.yml"
+            name = "fisherfaces.yaml"
         model.save(self.rec_path+name)
     
 #test = Picture_Manager()
 #test.get_samples("tr")    
+
