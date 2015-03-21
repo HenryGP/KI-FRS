5f6137375a13fc9faa59719c0cf47273277d3fde
henrygp <henrygp@henrygp>
2015-03-21 13:40:58 +0100

    Minor changes to implemented functions.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -18,7 +18,8 @@ manager = Picture_Manager()
     Based on the estimated value, processes the BW image
 """
 def light_estimation(rgb_img):
-    pass
+    V = cv.cvtColor(rgb_img,cv.COLOR_RGB2HSV)[:,:,2]
+    print "Mean value: ",cv.mean(V)[0]
 
 """
     Applies retina modeling algorithm to BW image
@@ -27,14 +28,16 @@ def retina_processing(bw_img):
     pass
 
 """
-   Gets all depth matrixes taken from the samples directory and normalizes it
+   Gets all depth matrixes taken from the samples directory and normalizes them
 """
 def normalize_depth(mode="tr"):
     samples,labels=manager.get_samples(mode,"mtx")
-    samples = samples.astype(np.float32)
-    for i in xrange(samples.shape[0]):
-        matrix = samples[i]
-        for p in xrange(matrix.shape[0]):
-            matrix[p]=0.1236 * math.tan(float(matrix[p]) / 2842.5 + 1.1863) * 100 #in centimeters
-        samples[i]=matrix
-    manager.save_samples(mode,"mtx",samples,labels)        
+    if not len(samples)==0:
+        samples = samples.astype(np.float32)
+        for i in xrange(samples.shape[0]):
+            matrix = samples[i]
+            for p in xrange(matrix.shape[0]):
+                matrix[p]=0.1236 * math.tan(float(matrix[p]) / 2842.5 + 1.1863) * 100 #in centimeters
+            samples[i]=matrix
+        manager.save_samples(mode,"mtx",samples,labels) 
+    return len(samples)
