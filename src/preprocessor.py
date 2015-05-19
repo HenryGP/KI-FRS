d3fb64ffe3790172c35f5e02a9c659eb93aca394
henrygp <henrygp@henrygp>
2015-05-19 17:40:39 +0200

    Addition of special features and documentation.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,8 +1,8 @@
 """
     Pre-processor for images
     Includes:
-    - Light-related algorithms
-    - Depth normalization algorithms
+    - Light-related algorithms: light estimation and normalization
+    - Depth normalization algorithms: matrix and depth image.
     - Auxiliaries
 """
 
@@ -10,28 +10,29 @@ import math
 import numpy as np
 from file_manager import Picture_Manager
 from file_manager import Sample_Manager
+from file_manager import External_DB_Manager
 import cv2 as cv
 
 manager = Picture_Manager()
 bw_detector = Sample_Manager().faceCascade
 depth_detector = Picture_Manager().faceCascade
 
-"""
-    Estimates light intensity in given RGB image
-    Based on the estimated value, processes the BW image
-    Returns True if the light estimation is OK, False in the other case
-"""
-# Under 80 is too dark, needs to be processed --> to be tested, just to guide a bit
-def light_estimation(rgb_img):
-    V = cv.cvtColor(rgb_img,cv.COLOR_RGB2HSV)[:,:,2]
-    if cv.mean(V)[0] < 80.0: #Needs to be processed
+def light_estimation(img):
+    """
+        Estimates light intensity in given RGB image
+        Based on the estimated value, processes the BW image
+        Returns True if the light estimation is OK, False in the other case
+    """
+    V = cv.cvtColor(img,cv.COLOR_RGB2HSV)[:,:,2]
+    # Under 80 is too dark, needs to be processed
+    if cv.mean(V)[0] < 80.0: 
         return False
     return True 
 
-"""
-   Gets all depth matrixes taken from the samples directory and normalizes them
-"""
 def normalize_depth_map(mode="tr"):
+    """
+       Gets all depth matrixes taken from the samples directory and normalizes them
+    """
     samples,labels, names=manager.get_samples(mode,"mtx")
     if not len(samples)==0:
         samples = samples.astype(np.float32)
@@ -43,16 +44,24 @@ def normalize_depth_map(mode="tr"):
         manager.save_samples(mode,"mtx",samples,labels) 
     return len(samples)
 
-def cut_image(bw_img,depth_img,depth_mtx):
+def cut_image(bw_img,depth_img=None,depth_mtx=None):
+    """
+        Using face detectors, cuts the B&W and depth images.
+        As depth face detector is inaccurate, bases the cutting pixels on the result of the B&W detector
+        THe last returned parameter indicates if the sample is suitable of not for using in the Recognizer
+    """
+    global counter
     result_bw=bw_img;result_mtx=depth_mtx;result_depth=depth_img;
-    faces = depth_detector.detectMultiScale(
-        depth_img,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(20, 20)
-    )
-    if len(faces)==0:
-        return result_bw, result_depth,result_mtx, True
+    if not depth_img==None:
+        # Face detection on depth image
+        faces = depth_detector.detectMultiScale(
+            depth_img,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(20, 20)
+        )
+        if len(faces)==0: #Only considers if at least one face was detected
+            return result_bw, result_depth,result_mtx, True
     faces = bw_detector.detectMultiScale(
         bw_img,
         scaleFactor=1.1,
@@ -61,33 +70,40 @@ def cut_image(bw_img,depth_img,depth_mtx):
     )
     for (x, y, w, h) in faces:
         result_bw = bw_img[y:y+h,x:x+w]
-        result_depth = depth_img[y:y+h,x:x+w]
-        result_mtx = depth_mtx[y:y+h,x:x+w]
+        if not depth_img==None:
+            result_depth = depth_img[y:y+h,x:x+w]
+            result_mtx = depth_mtx[y:y+h,x:x+w]
     if len(faces)==0:
         return result_bw, result_depth,result_mtx, True
     else:
         return result_bw, result_depth,result_mtx, False  
 
 def light_normalization(img):
-   #Histogram equalization
-   img = cv.equalizeHist(img)
-   #Gamma correction with factor 0.8 (smaller factors -> more bright)
-   img = img/255.0
-   img = cv.pow(img,0.8)
-   img = np.uint8(img*255)
-   img = cv.fastNlMeansDenoising(img,10,10,7,21)
-   #Gaussian filter to smooth
-   img = cv.GaussianBlur(img,(3,3),0)
-   return img
+    """
+        Normalizes light conditions in the given B&W image
+    """
+    #Histogram equalization
+    img = cv.equalizeHist(img)
+    #Gamma correction with factor 0.8 (smaller factors -> more bright)
+    img = img/255.0
+    img = cv.pow(img,0.8)
+    img = np.uint8(img*255)
+    img = cv.fastNlMeansDenoising(img,10,10,7,21)
+    #Gaussian filter to smooth
+    img = cv.GaussianBlur(img,(3,3),0)
+    return img
 
 def normalize_depth_image(depth_img,map_mtx):
+    """Normalizes the depth image by using a median blur filter"""
     return cv.medianBlur(depth_img,5)
 
 def normalize_images(mode="tr"):
+    """
+        Normalizes all data related to either training or testing, this includes depth, B&W and raw matrixes.
+        The result is a 100x100 image of the face.
+    """
     #RGB images for light estimation
     rgb_imgs, rgb_labels, rgb_names = manager.get_samples(mode,"rgb")
-    #print "RGB_IMGS SIZE: ",len(rgb_imgs)
-    #print "Unique labels: ",set(rgb_labels)
     #Raw depth images
     depth_imgs,depth_labels,depth_names = manager.get_samples(mode,"depth")
     #Depth map
@@ -133,25 +149,51 @@ def normalize_images(mode="tr"):
     manager.save_samples(mode,"depth",depth_new_imgs,rgb_new_labels,rgb_new_names)
     return len(rgb_new_labels)
 
-def normalize_sample(rgb_img,depth_img,depth_mtx):
-    bw_img = cv.cvtColor(rgb_img,cv.COLOR_RGB2GRAY)
+def normalize_sample(rgb_img,depth_img=None,depth_mtx=None,yale=False):
+    """
+        Given an individual sample, normalizes it by estimating light conditions
+        and later cutting and scaling it. The result is a 100x100 image of the face.
+    """
+    if not len(rgb_img.shape)==3:
+        bw_img = rgb_img
+        rgb_img = cv.cvtColor(rgb_img,cv.COLOR_GRAY2RGB)
+    else:
+        bw_img = cv.cvtColor(rgb_img,cv.COLOR_RGB2GRAY)
     if light_estimation(rgb_img)==False:
         bw_img = light_normalization(bw_img)        
-    r_bw,r_depth,r_mtx,rejected = cut_image(bw_img,depth_img,depth_mtx)
+    if not yale:
+        r_bw,r_depth,r_mtx,rejected = cut_image(bw_img,depth_img,depth_mtx)
+    else:
+        r_bw = bw_img;r_depth=None;r_mtx=None;rejected=False
     if rejected:
         return None,None,None
-    r_depth = normalize_depth_image(r_depth,r_mtx)
     r_bw = cv.resize(r_bw,(100,100))
-    r_depth = cv.resize(r_depth,(100,100))
+    if not depth_img==None:
+        r_depth = normalize_depth_image(r_depth,r_mtx)
+        r_depth = cv.resize(r_depth,(100,100))
     return r_bw,r_depth,r_mtx
     
+   
     
-
-#normalize_images("tr")
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
--- src/recognizer.py
+++ src/recognizer.py
@@ -1,4 +1,10 @@
-""" Module focused on face recognition """
+""" 
+    Module focused on face recognition 
+    Includes:
+        - Recognizer class: specialized for the KI-FRS
+            - Auxiliary options for graphics
+        - External DB Recognizer: modified version to use with external databases
+"""
 import numpy as np
 import cv2 as cv
 import os.path
@@ -6,8 +12,10 @@ import glob
 from itertools import chain
 from numpy.oldnumeric.linear_algebra import eigenvalues
 from file_manager import Picture_Manager
+from file_manager import External_DB_Manager
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import preprocessor
 
 """
     Main face recognition class, manager all the methods covered
@@ -18,53 +26,83 @@ class Recognizer():
     manager = Picture_Manager()
     types = {1: "Eigenfaces",2: "Fisherfaces"}
     sources = {"bw":"Raw B&W","depth":"Raw depth","nbw":"Normalized B&W","ndepth":"Normalized depth"}
-    num_components = {"nbw":[41,13],"ndepth":[17,13]}
     source=None
     
     """Initialization of model object through manager functionality"""
     def __init__(self,type,source=None,num_components=None):
         self.type = type
-        if source!=None:
+        if not source==None:
             self.source = source
-            self.model = self.manager.load_model(self.type,self.source,self.num_components[self.source][self.type-1])
+            self.model = self.manager.load_model(self.type,self.source)
+            if self.model == None:
+                if type==1:
+                    maxi = 100
+                else:
+                    maxi = 16
+                self.model = self.manager.load_model(self.type,self.source,self.optimum_components(type,source,5,maxi,False))
     
+    """Given a black&white and depth image, according to the selected mode, predicts the label"""               
     def predict(self,mode,bw_img,depth_img):
-        bw_model = self.manager.load_model(self.type,"nbw",self.num_components["nbw"][self.type-1])        
-        bw_prediction,bw_confidence = bw_model.predict(bw_img)
-        print "BW prediction: ",bw_prediction," ",bw_confidence
-        bw_model = self.manager.load_model(self.type,"ndepth",self.num_components["ndepth"][self.type-1])
-        depth_prediction,depth_confidence = bw_model.predict(depth_img)
-        print "Depth prediction: ",depth_prediction," ",depth_confidence
+        if mode=="auto":
+            bw_model = self.manager.load_model(self.type,"nbw")        
+            bw_prediction,bw_confidence = bw_model.predict(bw_img)
+            print "================================="
+            print "BW prediction: ",bw_prediction," ",bw_confidence
+            print "================================="
+            depth_model = self.manager.load_model(self.type,"ndepth")
+            depth_prediction,depth_confidence = depth_model.predict(depth_img)
+            print "================================="
+            print "Depth prediction: ",depth_prediction," ",depth_confidence
+            print "================================="
+            name,rgb_img,depth_img = self.manager.get_sample_info(bw_prediction)
+            cv.imshow(name,rgb_img)
+            name,rgb_img,depth_img = self.manager.get_sample_info(depth_prediction)
+        elif mode=="nbw":
+            bw_model = self.manager.load_model(self.type,"nbw")        
+            bw_prediction,bw_confidence = bw_model.predict(bw_img)
+            print "================================="
+            print "BW prediction: ",bw_prediction," ",bw_confidence
+            print "================================="
+            name,rgb_img,depth_img = self.manager.get_sample_info(bw_prediction)
+            cv.imshow(name,rgb_img)
+        else:
+            depth_model = self.manager.load_model(self.type,"ndepth")
+            depth_prediction,depth_confidence = depth_model.predict(depth_img)
+            print "================================="
+            print "Depth prediction: ",depth_prediction," ",depth_confidence
+            print "================================="
+            name,rgb_img,depth_img = self.manager.get_sample_info(depth_prediction)
+            cv.imshow(name,rgb_img)
+        cv.waitKey(0)
     
     """Training model method"""
-    def tr(self,source):
-        if self.source==None:
-            self.source=source
-            self.source=self.manager.load_model(self.type,self.source,num_components)
-        tr,labels,names=self.manager.get_samples("tr",source)
+    def tr(self,save=True):
+        tr,labels,names=self.manager.get_samples("tr",self.source)
         self.model.train(tr,labels)
-        self.manager.save_model(self.type,source,self.model)
+        if save:
+            self.manager.save_model(self.type,self.source,self.model)
         return
     
-    """Test model method, returns error percentage"""
-    def ts(self,source="bw"):
-        ts,labels,names = self.manager.get_samples("ts",source)
+    """Test model method, returns error percentage. Plot possibility available calling plot_results function"""
+    def ts(self,plot=False):
+        ts,labels,names = self.manager.get_samples("ts",self.source)
         images = ts.shape[0]; error = 0
         x=list(set(list(chain.from_iterable(labels.tolist()))))
         y = [0] * len(x)
         for img in xrange(ts.shape[0]):
             result,confidence=self.model.predict(ts[img])
-            print "======================================="
+            """print "======================================="
             print "Result: %s LABEL: %s" %(result,labels[img][0])
             print "Confidence: %.2f" % (confidence)
-            print "======================================="
-            
+            print "======================================="""        
             if not (result==int(labels[img][0])):
                 y[x.index(labels[img][0])]+=1
                 error +=1
-        #self.plot_results(x, y,(float(error)/images)*100.0)
+        if plot:
+            self.plot_results(x, y,(float(error)/images)*100.0)
         return (float(error)/images)*100.0
     
+    """Called from the ts function, creates an historam counting the number of false positives for each label"""
     def plot_results(self,x,y,error):
         N = len(x)
         ind = np.arange(N)
@@ -79,96 +117,159 @@ class Recognizer():
         plt.axis([1,len(x)+1,0,max(y)])
         plt.yticks(np.arange(0,max(y)+2,1))
         plt.show()
-               
-"""
-rec = Recognizer(1,"nbw",43)
-rec.tr("nbw")
-ret = rec.ts("nbw")
-"""
-
-#Test for eigenfaces to check recognition precision
-def test_components(type,source,min,max):
-    x = []; y=[]
-    for i in range(min,max+1):
-        rec=Recognizer(type,source,num_components=i)
-        rec.tr(source)
-        ret = rec.ts(source)
-        x.append(i); y.append(ret)
-    plt.plot(x,y)
-    plt.title("Components adjustment")
-    plt.xlabel("No. Components")
-    plt.ylabel("Error %")
-    plt.show()
-
-
-"""Code taken from repository: 
-https://github.com/Itseez/opencv/blob/2.4/samples/python2/facerec_demo.py#L124"""
-def normalize(X, low, high, dtype=None):
-    """Normalizes a given array in X to a value between low and high."""
-    X = np.asarray(X)
-    minX, maxX = np.min(X), np.max(X)
-    # normalize to [0...1].
-    X = X - float(minX)
-    X = X / float((maxX - minX))
-    # scale to [low...high].
-    X = X * (high-low)
-    X = X + low
-    if dtype is None:
-        return np.asarray(X)
-    return np.asarray(X, dtype=dtype)
-
-def model_testing(type,source):
-    if source == "nbw" and type==1:
-        comps = 43
-        shape = (100,100)
-    elif source == "ndepth" and type==1:
-        comps = 37
-        shape = (100,100)
-    elif source == "bw" and type==1:
-        comps = 43
-        shape = (192,256)
-    elif source=="depth"  and type==1:
-        comps = 84
-        shape = (192,256)
-    elif source == "nbw" and type==2:
-        comps = 13
-        shape = (100,100)
-    elif source == "ndepth" and type==2:
-        comps = 13
-        shape = (100,100)
-    elif source == "bw" and type==2:
-        comps = 13
-        shape = (192,256)
-    elif source=="depth"  and type==2:
-        comps = 15
-        shape = (192,256)
-    rec = Recognizer(type,source,comps)
-    rec.tr(source)
-    mean = rec.model.getMat("mean")
-    eigenvectors = rec.model.getMat("eigenvectors")
-    mean_norm = normalize(mean, 0, 255, dtype=np.uint8)
-    mean_resized = mean_norm.reshape(shape)
-    #cv.imshow("mean", mean_resized)
-    cv.imwrite("mean.png", mean_resized)
-    for i in xrange(16):
-        eigenvector_i = eigenvectors[:,i].reshape(shape)
-        eigenvector_i_norm = normalize(eigenvector_i, 0, 255, dtype=np.uint8)
-        #cv.imshow("eigenface_%d" % (i), eigenvector_i_norm)
-        cv.imwrite("eigenface_%d.png" % (i), eigenvector_i_norm)
-    cv.waitKey()
     
+    """Creates a test model, used for component adjustment"""
+    def create_model(self,type,source,num_components):
+        if type==1:    
+            self.model = cv.createEigenFaceRecognizer(num_components)
+        elif type==2:
+            self.model = cv.createFisherFaceRecognizer(num_components)
+    
+    """Calculates the optimum number of components to preserve given a certain range"""       
+    def optimum_components(self,type,source,mini,maxi,plot=False):
+        x = []; y=[]
+        for i in range(mini,maxi+1):
+            self.create_model(type, source, i)
+            self.tr(False)
+            ret = self.ts()
+            x.append(i); y.append(ret)
+        if plot==True:
+            plt.plot(x,y)
+            plt.title("Components adjustment")
+            plt.xlabel("No. Components")
+            plt.ylabel("Error %")
+            plt.show()
+        return x[y.index(min(y))]
+       
+    """Code taken from repository: 
+    https://github.com/Itseez/opencv/blob/2.4/samples/python2/facerec_demo.py#L124"""
+    def normalize(self,X, low, high, dtype=None):
+        """Normalizes a given array in X to a value between low and high."""
+        X = np.asarray(X)
+        minX, maxX = np.min(X), np.max(X)
+        # normalize to [0...1].
+        X = X - float(minX)
+        X = X / float((maxX - minX))
+        # scale to [low...high].normalize
+        X = X * (high-low)
+        X = X + low
+        if dtype is None:
+            return np.asarray(X)
+        return np.asarray(X, dtype=dtype) 
+
+    """Displays the number of eigenvectors specified and the mean face"""       
+    def show_eigenvectors(self,num_components):
+        mean = self.model.getMat("mean")
+        eigenvectors = rec.model.getMat("eigenvectors")
+        mean_norm = self.normalize(mean, 0, 255, dtype=np.uint8)
+        mean_resized = mean_norm.reshape((100,100))
+        cv.imwrite("mean.png", mean_resized)
+        #cv.imshow("mean face", mean_resized)
+        for i in xrange(num_components+1):
+            eigenvector_i = eigenvectors[:,i].reshape((100,100))
+            eigenvector_i_norm = self.normalize(eigenvector_i, 0, 255, dtype=np.uint8)
+            cv.imshow("eigenface_%d" % (i), eigenvector_i_norm)
+            #cv.imwrite("eigenface_%d.png" % (i), eigenvector_i_norm)
+        cv.waitKey()
+
+"""
+rec = Recognizer(2,"ndepth")
+rec.tr()
+ret = rec.ts(True)
+print "Percentage: %f"%(ret)
+"""     
+
+"""Specific class to manage external databases testing"""   
+class ExternalDB_Recognizer(Recognizer):
+    """Initialization of a model given a partition percentage to create the training and testing sets"""
+    def __init__(self,type,database,partition_percentage,num_components=None):
+        if not num_components==None:
+            if type==1:
+                self.model = cv.createEigenFaceRecognizer(num_components)
+            else:
+                self.model = cv.createFisherFaceRecognizer(num_components) 
+        else:
+            if type==1:
+                self.model = cv.createEigenFaceRecognizer()
+            else:
+                self.model = cv.createFisherFaceRecognizer()
+        self.manager = External_DB_Manager(partition_percentage)
+        self.database = database
+    
+    """For benchmarking the algorithm performance, first trains the model and then tests it"""
+    def tr_and_ts(self,filter=True):
+        tr_samples,tr_labels,ts_samples,ts_labels = self.manager.get_data(self.database) 
+        self.tr(tr_samples,tr_labels,filter)
+        return self.ts(ts_samples,ts_labels,filter)
+    
+    """Training the model with the given training samples and labels, has the possibility of filtering the set"""
+    def tr(self,tr_samples,tr_labels,filter=True):
+        print "Training model of %s raw samples"%(str(len(tr_samples)))
+        samples = []; labels =[]; accepted=0
+        for i in xrange(len(tr_samples)):
+            sample = cv.imread(tr_samples[i],0)
+            if (not sample==None) and (filter) :
+                sample, d1, d2 = preprocessor.normalize_sample(sample,yale=(self.database=="yale"))
+            elif not sample==None:    
+                sample = cv.resize(sample,(100,100))
+            if not sample==None:
+                accepted+=1
+                try:    
+                    samples = np.vstack((samples,sample.reshape(sample.shape[0]*sample.shape[0])))
+                    labels = np.vstack((labels,tr_labels[i]))
+                except:
+                    samples = sample.reshape(sample.shape[0]*sample.shape[0])
+                    labels = tr_labels[i]        
+        print "Tr processed samples: ",accepted
+        self.model.train(samples,labels)
+    
+    """Tests the trained model. The testing set could be also filtered or not by the preprocessor"""
+    def ts(self,ts_samples,ts_labels,filter=True):
+        print "Ts raw samples: ",len(ts_samples)
+        error = 0; accepted = 0
+        for i in xrange(len(ts_samples)):
+            sample = cv.imread(ts_samples[i],0)
+            if (not sample==None) and (filter):
+                sample, d1, d2 = preprocessor.normalize_sample(sample,yale=(self.database=="yale"))
+            elif not sample==None:
+                sample = cv.resize(sample,(100,100))
+            if not sample==None:
+                accepted += 1
+                result,confidence=self.model.predict(sample.reshape(sample.shape[0]*sample.shape[0]))
+                """print "======================================="
+                print "Result: %s LABEL: %s" %(result,ts_labels[i])
+                print "Confidence: %.2f" % (confidence)
+                print "======================================="""
+                if not result==ts_labels[i]:
+                    error +=1
+        print "Ts processed samples: ",accepted
+        print "Hit rate: ",100.0-(float(error)/accepted)*100.0
+        return 100.0-(float(error)/accepted)*100.0
 
 
+#rec = ExternalDB_Recognizer(2,"yale",70)
+#rec.tr_and_ts(False)
 
 
-
-
-
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
     
